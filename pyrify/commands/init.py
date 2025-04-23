e2da1f9e0b6a3a3a3f2bd17107aa8b081ff02b93
mutantsan <kvaqich@gmail.com>
2025-04-23 22:18:13 +0300

    feat: basic implementation for postgres

--- pyrify/commands/init.py
+++ pyrify/commands/init.py
@@ -1,46 +1,50 @@
 import click
-import sqlalchemy as sa
+import yaml
+
+import pyrify.utils as utils
 
 
 @click.command()
-@click.option("--db-url", type=str, required=True)
+@click.option("-d", "--db-url", type=str, required=True)
 def init(db_url: str):
     """Initializes the sanitize configuration"""
-    click.echo(f"Initializing sanitize configuration for {db_url}")
-    engine = sa.create_engine(db_url)
+    engine = utils.get_db_engine(db_url)
 
-    with engine.connect() as conn:
-        conn.execute(sa.text("SELECT 1"))
-        click.echo("Database connected successfully")
+    if not engine:
+        raise click.ClickException("Database connection failed")
+    database_structure = {}
 
-    tables = get_tables_list(engine)
+    for table in utils.get_tables_list(engine):
+        database_structure.setdefault(table, {})
 
-    for table in tables:
-        click.echo(f"Table: {table}")
-
-        columns = get_table_columns(engine, table)
-        click.echo(f"Columns: {columns}")
-
-
-def get_tables_list(engine: sa.Engine) -> list[str]:
-    with engine.connect() as conn:
-        return [
-            row[0]
-            for row in conn.execute(
-                sa.text(
-                    "SELECT table_name FROM information_schema.tables WHERE table_schema='public'"
-                )
-            ).fetchall()
+        database_structure[table]["columns"] = [
+            {column: "~"} for column in utils.get_table_columns(engine, table)
         ]
 
+    generate_yaml_config(database_structure)
 
-def get_table_columns(engine: sa.Engine, table_name: str) -> list[str]:
-    with engine.connect() as conn:
-        return [
-            row[0]
-            for row in conn.execute(
-                sa.text(
-                    f"SELECT column_name FROM information_schema.columns WHERE table_name='{table_name}'"
-                )
-            ).fetchall()
-        ]
+
+def get_yaml_usage_comment() -> str:
+    return """
+# This is a YAML configuration file for the pyrify tool.
+# It describes the tables and the columns that should be sanitized.
+
+# You can delete the table from the list to keep it as is.
+# You can delete the column from the list to keep it as is.
+
+# You can use the following commands relative to the table name:
+# - clean: clean the table
+# - drop: drop the table
+
+# You can use different strategies for each column:
+# - nullify: set the column to null
+# - fake_name: set the column to a fake name
+# - fake_email: set the column to a fake email
+# - fake_password: set the column to a fake password
+"""
+
+
+def generate_yaml_config(database_structure: dict[str, list[str]]) -> None:
+    click.echo(get_yaml_usage_comment())
+
+    click.echo(yaml.dump(database_structure))

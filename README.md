8a80789e96704f5effb96f429a29184a0b3dae23
mutantsan <kvaqich@gmail.com>
2025-04-24 16:54:30 +0300

    feat: implement mysql driver, refactor

--- README.md
+++ README.md
@@ -8,4 +8,28 @@ A CLI tool for database sanitization
 pip install pyrify
 ```
 
+## Initialize the sanitize config
 
+### Postgres
+
+By providing the database URI, the tool will automatically generate a sanitize config file.
+
+```sh
+pyrify init -d "postgresql://user:pass@localhost/db_name" > config.yml
+```
+
+### MySQL
+
+```sh
+pyrify init -d "mysql+pymysql://user:pass@localhost/db_name" > config.yml
+```
+
+## Configure the sanitize config
+
+The `init` command will create a config file with the following structure:
+
+```yaml
+```
+
+You have to specify the tables and columns that you want to sanitize. If you 
+don't need to sanitize a table or a column, you can remove it from the config file.
--- pyrify/utils.py
+++ pyrify/utils.py
@@ -1,107 +1,22 @@
 from pathlib import Path
-from typing import Any, Callable
 
-import sqlalchemy as sa
+import click
 import yaml
 
-import pyrify.strategy as strategy
 import pyrify.types as types
-
-
-def get_db_engine(db_uri: str) -> sa.Engine | None:
-    """Get a SQLAlchemy engine for the given database URI
-
-    Args:
-        db_uri: The database URI
-
-    Returns:
-        An SQLAlchemy engine or None if the connection failed
-    """
-    engine = sa.create_engine(db_uri)
-
-    with engine.connect() as conn:
-        if not conn.execute(sa.text("SELECT 1")).fetchone():
-            return None
-
-    return engine
-
-
-def get_tables_list(engine: sa.Engine) -> list[str]:
-    """Get the list of tables in the database
-
-    Args:
-        engine: The SQLAlchemy engine to use
-
-    Returns:
-        A list of table names
-    """
-    with engine.connect() as conn:
-        return [
-            row[0]
-            for row in conn.execute(
-                sa.text(
-                    "SELECT table_name FROM information_schema.tables WHERE table_schema='public'"
-                )
-            ).fetchall()
-        ]
-
-
-def get_table_columns(engine: sa.Engine, table_name: str) -> list[str]:
-    """Get the list of columns in a table
-
-    Args:
-        engine: The SQLAlchemy engine to use
-        table_name: The name of the table to get the columns of
-
-    Returns:
-        A list of column names
-    """
-    with engine.connect() as conn:
-        return [
-            row[0]
-            for row in conn.execute(
-                sa.text(
-                    f"SELECT column_name FROM information_schema.columns WHERE table_name='{table_name}'"
-                )
-            ).fetchall()
-        ]
-
-
-def drop_table(engine: sa.Engine, table_name: str) -> None:
-    with engine.connect() as conn:
-        conn.execute(sa.text(f'DROP TABLE IF EXISTS "{table_name}" CASCADE'))
-        conn.commit()
-
-
-def clean_table(engine: sa.Engine, table_name: str) -> None:
-    with engine.connect() as conn:
-        conn.execute(sa.text(f'TRUNCATE TABLE "{table_name}" RESTART IDENTITY CASCADE'))
-        conn.commit()
-
-
-def update_column(
-    engine: sa.Engine, table_name: str, column_name: str, strategy: Callable[[Any], str]
-) -> None:
-
-    with engine.connect() as conn:
-        rows = conn.execute(
-            sa.text(f'SELECT "{column_name}" FROM "{table_name}"')
-        ).fetchall()
-
-        for row in rows:
-            if row[0] is not None:
-                conn.execute(
-                    sa.text(
-                        f'UPDATE "{table_name}" '
-                        f'SET "{column_name}" = {strategy(row[0])} '
-                        f"WHERE \"{column_name}\" = '{row[0]}'"
-                    )
-                )
-
-        conn.commit()
+from pyrify.drivers import BaseDriver, MySQLDriver, PostgresDriver
+from pyrify.strategies import BaseStrategy, get_strategies
 
 
 def load_config(config_path: Path) -> types.SanitizeConfig:
+    """Load the sanitize configuration from a YAML file
+
+    Args:
+        config_path: The path to the YAML file
+
+    Returns:
+        A sanitize configuration
+    """
     with open(config_path) as f:
         raw_config = yaml.safe_load(f)
 
@@ -113,7 +28,7 @@ def load_config(config_path: Path) -> types.SanitizeConfig:
     return sanitize_config
 
 
-def get_strategy(strategy_name: str) -> Callable[[str], str]:
+def get_strategy(strategy_name: str) -> type[BaseStrategy]:
     """Get a strategy function by its name
 
     Args:
@@ -122,8 +37,72 @@ def get_strategy(strategy_name: str) -> Callable[[str], str]:
     Returns:
         A strategy function
     """
-    strategies = strategy.get_strategies()
+    strategies = get_strategies()
+
     if strategy_name not in strategies:
         raise ValueError(f"Invalid strategy: {strategy_name}")
 
     return strategies[strategy_name]
+
+
+def get_driver(db_uri: str) -> BaseDriver:
+    """Get a driver for the given database URI
+
+    Args:
+        db_uri: The database URI
+
+    Returns:
+        A database driver
+    """
+    if db_uri.startswith("postgres"):
+        return PostgresDriver(db_uri)
+    elif db_uri.startswith("mysql"):
+        return MySQLDriver(db_uri)
+    else:
+        raise ValueError(f"Unsupported database URI: {db_uri}")
+
+
+def sanitize_db(driver: BaseDriver, sanitize_config: types.SanitizeConfig) -> None:
+    for table, table_config in sanitize_config.tables.items():
+        if table not in driver.tables:
+            click.echo(f"Table {table} not found. Skipping...")
+            continue
+
+        if table_config.clean:
+            click.echo(f"Cleaning table {table}...")
+            driver.clean_table(table)
+            continue
+
+        if table_config.drop:
+            click.echo(f"Dropping table {table}...")
+            driver.drop_table(table)
+            continue
+
+        if not table_config.columns:
+            continue
+
+        for column_name, strategy in table_config.columns.items():
+            if column_name not in driver.columns[table]:
+                click.echo(
+                    f"Column {column_name} for table {table} not found. Skipping..."
+                )
+                continue
+
+            click.echo(
+                f"Updating column {column_name} for table {table} with strategy {strategy}..."
+            )
+
+            # Handle nested strategies
+            if isinstance(strategy, dict):
+                strategy_name = strategy["strategy"]
+                strategy_kwargs = strategy.pop("kwargs", {})
+            else:
+                strategy_name = strategy
+                strategy_kwargs = {}
+
+            driver.update_column(
+                table,
+                column_name,
+                get_strategy(strategy_name),
+                **strategy_kwargs,
+            )
